ead954682f0eb7b58b69a42ac7c7c8633dfc4039
paul960 <psinchuk@outlook.com>
2020-08-12 09:56:49 -0400

    Add files via upload

---
+++ RDS_Certs.ps1
@@ -0,0 +1,11 @@
+﻿param($result)
+$thumbprint = $result.ManagedItem.CertificateThumbprintHash
+Set-RDCertificate -Role RDRedirector -Thumbprint $thumbprint -Force
+Set-RDCertificate -Role RDGateway -Thumbprint $thumbprint -Force
+Set-RDCertificate -Role RDPublishing -Thumbprint $thumbprint -Force
+Set-RDCertificate -Role RDWebAccess -Thumbprint $thumbprint -Force
+
+Restart-Service TSGateway
+Restart-Service SessionEnv
+Restart-Service Tssdis
+Restart-Service RDMS
