083011e55467ededd62142fab440264bb4324751
paul960 <psinchuk@outlook.com>
2020-11-05 17:54:58 -0500

    Added Disabled OU and auto confirm

--- Disabled_Users_Remove_Groups.ps1
+++ Disabled_Users_Remove_Groups.ps1
@@ -1,5 +1,6 @@
-foreach($aduser in (get-aduser -filter "enabled -eq 'false'" -Properties memberof)){
+$DisabledOU = "DistinguishedName of Disabled OU"
+foreach($aduser in (get-aduser -filter "enabled -eq 'false'" -SearchBase $DisabledOU -Properties memberof)){
     $aduser.memberof |
     where name -ne 'domain users' |
-    Remove-ADGroupMember -Members $aduser
+    Remove-ADGroupMember -Members $aduser -Confirm:$false
 }
